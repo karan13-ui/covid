9c965de3da78ee00114ecc6fe072d8fb4fdef2ed
atulyagupta2712 <36509176+atulyagupta2712@users.noreply.github.com>
2020-05-07 00:07:41 +0530

    Updates

---
+++ precache-manifest.c57453156f10d22c8c1211c9e4333719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9938893758dcb3495f2d8c509bfb3a",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "a5ebfb6bfe818c938f1a",
+    "url": "/covid/static/css/main.d19f4e70.chunk.css"
+  },
+  {
+    "revision": "9aa4a89d9d45b527673a",
+    "url": "/covid/static/js/2.059752fd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.059752fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ebfb6bfe818c938f1a",
+    "url": "/covid/static/js/main.2a3d8881.chunk.js"
+  },
+  {
+    "revision": "8fa97ab77468439a0938",
+    "url": "/covid/static/js/runtime-main.67a237fe.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/image.d7265326.png"
+  }
+]);
